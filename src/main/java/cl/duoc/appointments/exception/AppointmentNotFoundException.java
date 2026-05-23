04f741d0f73057c814230b42ae9542e250dda88a
polirritmico <ebray187@gmail.com>
2026-05-23 01:24:07 -0400

    feat(AppointmentNotFoundException): add domain exception

---
+++ src/main/java/cl/duoc/appointments/exception/AppointmentNotFoundException.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.appointments.exception;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AppointmentNotFoundException extends RuntimeException {
+    public AppointmentNotFoundException(Long appointmentId) {
+        String msg = "Appointment with id '" + appointmentId + "' not fount in the DB.";
+        log.error(msg);
+        super(msg);
+    }
+}
