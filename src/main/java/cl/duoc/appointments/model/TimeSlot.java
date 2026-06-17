25d37acb4c2aaae12881520e9d829f68a426fc78
polirritmico <ebray187@gmail.com>
2026-06-16 23:56:23 -0400

    feat(TimeSlot): add model

---
+++ src/main/java/cl/duoc/appointments/model/TimeSlot.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.appointments.model;
+
+import java.time.LocalTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TimeSlot {
+    private LocalTime startTime;
+    private LocalTime endTime;
+}
